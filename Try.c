da868c55e1010b92745d70a6984c6969b23e5d7f
HubGitterHere <schmidj4@oregonstate.edu>
2026-05-06 03:22:50 +0000

    changed ThreadVideo01-Start.c to Try.c

---
+++ Try.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define MAX 5
+
+void *printBanner1(void *arg)
+{
+	int times = 20;
+	int rand_value;
+	for (int i=0; i<times; i++) {
+		printf("Thread 1 working ... ");
+        rand_value = (rand() % MAX)*100000;
+		printf("waiting %d microseconds\n",rand_value);
+        usleep(rand_value); //sleep time in microseconds
+    }
+    return NULL;
+}
+void *printBanner2(void *arg)
+{
+	int times = 20;
+	int rand_value;
+	for (int i=0; i<times; i++) {
+		printf("                    Thread 2 working ... ");
+        rand_value = (rand() % MAX)*100000;
+		printf("waiting %d microseconds\n",rand_value);
+        usleep(rand_value); //sleep time in microseconds
+    }
+    return NULL;
+}
+int main(int argc, char *argv[])
+{
+    pthread_t p1, p2;
+
+    pthread_create(&p1, NULL, printBanner1, (void *) NULL);
+    pthread_create(&p2, NULL, printBanner1, (void *) NULL);
+    pthread_join(p2, NULL);
+    pthread_join(p1, NULL);
+    //*printBanner1((void *) NULL);
+    return 0;
+}
