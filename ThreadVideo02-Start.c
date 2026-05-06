f4f3f74e2400bd110df66cf3c679a333b7692103
HubGitterHere <schmidj4@oregonstate.edu>
2026-05-06 02:08:46 +0000

    added starter files for threads

---
+++ ThreadVideo02-Start.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdint.h>
+//#include <unistd.h>
+#include <windows.h>
+#include <stdlib.h>
+#define MAX 5
+
+int addArray(int *iArray)
+{
+    int total = 0;
+    for (int i = 0; i < MAX; i++) {
+		printf("adding array[%d] = %d  ...\n", i, *(iArray+i));
+	    total += *(iArray+i);
+    }
+    return total;
+}
+
+int main(int argc, char *argv[])
+{
+	int array[MAX];
+	int result;
+
+    for (int i = 0; i < MAX; i++) {
+		array[i] = 2*i+1;
+	}
+
+    result = addArray(array);
+	printf("The sum of the array elements is = %d.\n", result);
+
+    return 0;
+}
