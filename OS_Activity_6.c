ab69de071ec0b6d6a9042bb075305e1d3235077f
HubGitterHere <schmidj4@oregonstate.edu>
2026-05-10 02:41:46 +0000

    finished OS_Activity_6 program

--- OS_Activity_6.c
+++ OS_Activity_6.c
@@ -4,68 +4,63 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#define MAX 5
+#define MAX 15
 //pthread_mutex_t lockRand = PTHREAD_MUTEX_INITIALIZER;
 sem_t semaphore;
 
-volatile int schedules[] = {6, 7, 8};
+volatile int currentBudget = 50 * 100000;
 
-void *checkSchedule1(void *arg)
+void *checkBudget1(void *arg)
 {
-	int times = 20;
+	int times = 10;
 	int rand_value;
+	for (int i=0; i<times; i++) {
+        sem_wait(&semaphore);
 		printf("Thread 1 working ... ");
-        rand_value = (rand() % MAX);
-		printf("waiting %d seconds\n",rand_value);
-        usleep(rand_value*100000); //sleep time in microseconds
-
+        rand_value = (rand() % MAX)*100000;
+        printf("spending %d.", rand_value);
+        if (currentBudget >= rand_value){
+            currentBudget -= rand_value;
+            printf(" Money spent, ");
+        }else{
+            printf(" Can't spend, ");
+        }
+		printf("current budget is %d\n",currentBudget);
+        sem_post(&semaphore);
+        usleep(rand_value); //sleep time in microseconds
+    }
     return NULL;
 }
-void *checkSchedule2(void *arg)
+void *checkBudget2(void *arg)
 {
-	int times = (int) arg;
+	int times = 10;
 	int rand_value;
-		printf("                    Thread 2 working ... ");
-        rand_value = (rand() % MAX);
-		printf("waiting %d seconds\n",rand_value);
-        usleep(rand_value*100000); //sleep time in microseconds
-    return NULL;
-}
-void *checkSchedule3(void *arg)
-{
-	int times = 20;
-	int rand_value;
-		printf("                                    Thread 3 working ... ");
-        rand_value = (rand() % MAX);
-		printf("waiting %d seconds\n",rand_value);
-        usleep(rand_value*100000); //sleep time in microseconds
-    return NULL;
-}
-void *checkSchedule4(void *arg)
-{
-	int times = (int) arg;
-	int rand_value;
-		printf("                                                    Thread 4 working ... ");
-        rand_value = (rand() % MAX);
-		printf("waiting %d seconds\n",rand_value);
-        usleep(rand_value*100000); //sleep time in microseconds
+	for (int i=0; i<times; i++) {
+        sem_wait(&semaphore);
+		printf("        Thread 2 working ... ");
+        rand_value = (rand() % MAX)*100000;
+        printf("spending %d.", rand_value);
+        if (currentBudget >= rand_value){
+            currentBudget -= rand_value;
+            printf(" Money spent, ");
+        }else{
+            printf(" Can't spend, ");
+        }
+		printf("current budget is %d\n",currentBudget);
+        sem_post(&semaphore);
+        usleep(rand_value); //sleep time in microseconds
+    }
     return NULL;
 }
 int main(int argc, char *argv[])
 {
-    int times = 5;
-
     sem_init(&semaphore, 0, 1);
 
-    pthread_t p1, p2, p3, p4;
-    pthread_create(&p1, NULL, checkSchedule1, (void *) NULL);
-    pthread_create(&p2, NULL, checkSchedule2, (void *) NULL);
-    pthread_create(&p3, NULL, checkSchedule3, (void *) NULL);
-    pthread_create(&p4, NULL, checkSchedule4, (void *) NULL);
+    pthread_t p1, p2;
+    pthread_create(&p1, NULL, checkBudget1, NULL);
+    pthread_create(&p2, NULL, checkBudget2, NULL);
     // join waits for the threads to finish
     pthread_join(p1, NULL);
     pthread_join(p2, NULL);
-    pthread_join(p3, NULL);
-    pthread_join(p4, NULL);
     return 0;
 }
