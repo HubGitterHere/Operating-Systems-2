cbeb64882c58268b4eecf85574ff2ee3090ab2c6
HubGitterHere <schmidj4@oregonstate.edu>
2026-05-21 06:47:16 +0000

    Started adding SCAN array

--- OS_Activity_8.c
+++ OS_Activity_8.c
@@ -3,28 +3,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
+#include <string.h>
 #define MAX 15
 
 
 
-int scheduling(int arr[], int size){
+void FCFSscheduling(int arr[], int size){
     int diskHead = arr[0];
     int movement = 0;
+    printf("current movements: ");
     for(int i=1; i< size; i++){
         movement += abs(diskHead-arr[i]);
         diskHead = arr[i];
-        printf("current movement: %d\n", movement);
+        printf("%d, ", movement);
     }
-    return movement;
+    printf("\n");
 }
-int sorting(int arr[], int size){
-    int diskHead = arr[0];
-    int *lowerArr = (int*) malloc(size *sizeof(int));
-    int lowerArrSize = 0;
-    for(int i=1; i<size; i++){
-        if(arr[i] < diskHead){
-            
+//based off of AI overview
+void sorting(int arr[], int size){
+    for(int i = 0; i<size; i++){
+        for(int j=0; j< size-1;j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]=temp;
+            }
         }
     }
 
@@ -33,13 +36,20 @@ int main(int argc, char *argv[])
 {
     int size = 10;
     int FCFS_array[] = {228,46,83,234,245,103,156,133,174,64};
-    
+    //print first come first serve scheduling
     printf("First come first serve scheduling\n");
-    scheduling(FCFS_array, size);
+    FCFSscheduling(FCFS_array, size);
+    //print SCAN scheduling
+    int SCAN_array[size];
+    for (int i=0; i<size; i++){
+        SCAN_array[i] = FCFS_array[i];
+    }
+    sorting(SCAN_array, size);
+    int diskHead = FCFS_array[0];
 
-    int SCAN_array[] = {228,174,156,133,103,83,64,46,234,245};
-    printf("Scan scheduling\n");
-    scheduling(SCAN_array, size);
+    for (int i = 0; i < size; i++){
+        printf("%d\n", SCAN_array[i]);
+    }
     
     return 0;
 }
